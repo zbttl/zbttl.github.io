30b863792297c037b291771ace2387a943bf1099
zbttl <zbttl@foxmail.com>
2021-01-13 19:55:46 +0800

    Site updated: 2021-01-13 19:55:22

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
   $("body").click(function(e) {
     var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
-    var $i = $("<span/>").text(a[a_idx]);
+    var $i = $("<span>").text(a[a_idx]);
     var x = e.pageX,
       y = e.pageY;
     $i.css({
@@ -21,4 +21,4 @@ jQuery(document).ready(function($) {
     });
     a_idx = (a_idx + 1) % a.length;
   });
-});
+});</span>
